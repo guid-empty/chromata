6fe49582b046f1f33c00b4fe913a96a22d4bff46
andrey.smirnov <guid.empty@gmail.com>
2021-12-07 18:52:42 +0300

    arrays

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,8 @@ module.exports = {
             { from: './src/index.html' },
             { context: './src', from: 'assets/**/*' }
         ])
-    ]
+    ],
+    optimization: {
+            minimize: false
+        },
 };
